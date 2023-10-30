bb4b3c01f40c892a98567ff990cb9a39e250a8ad
Phil Nocerini <nocerini@gmail.com>
2023-10-30 15:51:09 -0400

    Monday Assignments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios'
+import {useState} from 'react'
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  const getPokemon = () => {
+    console.log("Axios button")
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then (response => {
+        setPokemon(response.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon via Axios</h1>
+      <button onClick={getPokemon}>Fetch Pokemon</button>
+      <div>
+        <ul>
+          {
+            pokemon.map((mon, i) => {
+              return <li key={i}>{mon.name}</li>
+            }
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
